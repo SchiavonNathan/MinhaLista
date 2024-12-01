deb644bda0999961ad4c6b13064d6a6da17e1021
SchiavonNathan <Nathanmestrepk@gmail.com>
2024-12-01 17:27:05 -0300

    Estilizacao login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,63 @@
 import React from "react";
 
 import {
-    Text
+    Text,
+    View,
+    Image,
+    TextInput,
+    TouchableOpacity
 } from 'react-native';
+
+import { themas } from "../../global/themes";
+import Logo from "../../assets/logo.png"
+import {style} from "./styles"
+import {MaterialIcons} from "@expo/vector-icons"
 
 export default function login (){
     return(
-        <Text>Ola otarios</Text>
+        <View style = {style.container}>
+            <View style = {style.boxTop}>
+                <Image 
+                    source={Logo}
+                    style = {style.logo}
+                    resizeMode="contain"
+                />
+                <Text style = {style.text}>Bem vindo de volta!</Text>
+            </View>
+
+            <View style = {style.boxMid}>
+                <Text style = {style.titleInput}>Email</Text>
+                <View style = {style.boxInput}>
+                    <TextInput
+                        style = {style.input}
+                    />
+                    <MaterialIcons
+                        name="email"
+                        size={20}
+                        color={themas.colors.gray}
+                    />
+                </View>
+                <Text style = {style.titleInput}>Senha</Text>
+                <View style = {style.boxInput}>
+                    <TextInput
+                        style = {style.input}
+                    />
+                    <MaterialIcons
+                        name="remove-red-eye"
+                        size={20}
+                        color={themas.colors.gray}
+                    />
+                </View>
+            </View>
+
+            <View style = {style.boxBottom}>
+                <TouchableOpacity style={style.button}>
+                    <Text style={style.textButton}>Entrar</Text>    
+                </TouchableOpacity> 
+            </View>
+
+            <Text style={style.textBottom}>Não tem conta? <Text style={{color:themas.colors.primary}}>Crie agora!</Text></Text>
+
+        </View>
     )
 }
---
+++ src/pages/login/styles.tsx
@@ -0,0 +1,86 @@
+import { Dimensions, StyleSheet } from "react-native";
+import { themas } from "../../global/themes";
+
+export const style = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    boxTop:{
+        height:Dimensions.get('window').height/3,
+        width:"100%",
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    boxMid:{
+        height:Dimensions.get('window').height/4,
+        width:"100%",
+        paddingHorizontal: 37
+    },
+    boxBottom:{
+        height:Dimensions.get('window').height/3,
+        width:"100%",
+        alignItems:"center",
+        paddingTop:40
+    },
+    logo:{
+        width:80,
+        height:80
+    },
+    text:{
+        fontWeight:"bold",
+        marginTop:40,
+        fontSize:18
+    },
+    titleInput:{
+        marginLeft: 5,
+        marginTop: 20,
+        color: themas.colors.gray
+    },
+    boxInput:{
+        width:"100%",
+        height:40,
+        borderWidth:1,
+        borderRadius:40,
+        marginTop:10,
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 7,
+        backgroundColor: themas.colors.lightGray,
+        borderColor: themas.colors.lightGray
+    },
+    input:{
+        height: "100%",
+        width: "90%",
+        borderRadius:40,
+        paddingLeft:5
+    },
+    button:{
+        width: 230,
+        height: 50,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: themas.colors.primary,
+        borderRadius: 40,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 12,
+        },
+        shadowOpacity: 0.58,
+        shadowRadius: 16.00,
+
+        elevation: 24,
+    },
+    textButton:{
+        fontSize:16,
+        color:"#FFFF",
+        fontWeight:"bold"
+    },
+    textBottom:{
+        fontSize:16,
+        color: themas.colors.gray
+    }
+    
+})
