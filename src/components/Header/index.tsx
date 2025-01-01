33f14e6c6b87d13e7b815b47df151e15dbc355cb
SchiavonNathan <Nathanmestrepk@gmail.com>
2025-01-01 17:02:36 -0300

    Atualizado component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import { style } from "./styles";
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function Header() {
     return(
         <View style = {style.container}>
-             <Ionicons name="arrow-back-outline" size={25} color="gray" />
+             <TouchableOpacity>
+                <Ionicons name="arrow-back-outline" size={25} color="gray" />
+             </TouchableOpacity>
         </View>
     )
 }
